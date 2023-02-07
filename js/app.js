ef2775e3880e8ca13ad6f2219e99c242723e1c6c
kuliznikovjeka <99824934+kuliznikovjeka@users.noreply.github.com>
2023-02-07 22:40:05 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -3277,6 +3277,26 @@
             }));
         }));
     }
+    document.addEventListener("DOMContentLoaded", (() => {
+        const accrodions = document.querySelectorAll(".accordion__item");
+        accrodions.forEach((el => {
+            el.addEventListener("click", (e => {
+                const self = e.currentTarget;
+                const control = self.querySelector(".accordion__control");
+                const content = self.querySelector(".accordion__content");
+                self.classList.toggle("_open");
+                if (self.classList.contains("_open")) {
+                    control.setAttribute("aria-expanded", true);
+                    content.setAttribute("aria-hidden", false);
+                    content.style.maxHeight = content.scrollHeight + "px";
+                } else {
+                    control.setAttribute("aria-expanded", false);
+                    content.setAttribute("aria-hidden", true);
+                    content.style.maxHeight = null;
+                }
+            }));
+        }));
+    }));
     window["FLS"] = true;
     isWebp();
     menuInit();
